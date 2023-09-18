e371cc70dcece78612e72bcf7e70ef9b2fce5a7b
Len <mail@lenblum.de>
2023-09-18 16:41:06 +0200

    fixing issues in flutter

--- lib/main.dart
+++ lib/main.dart
@@ -2,23 +2,24 @@ import 'package:cafeminute/API/nots.dart';
 import 'package:cafeminute/firebase_options.dart';
 import 'package:cafeminute/news/news_view.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'API/initInventory.dart';
 import 'API/utils.dart';
 import "main_page.dart";
-import 'package:firebase_messaging/firebase_messaging.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
   await init();
-  await initNots();
   getRegistrations();
   runApp(Minute());
+    await Firebase.initializeApp(
+  name: "cafeminute",
+  options: DefaultFirebaseOptions.currentPlatform,
+);
 }
 
-var url = "http://192.168.178.32:1312";
+var url = "http://minutebackend.hopto.org:1312";
 
 class Minute extends StatefulWidget {
   const Minute({Key? key}) : super(key: key);
